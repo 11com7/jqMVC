adafd4cda9952ba5093d23660613eb433950d181
Dominik Pesch <d.pesch@11com7.de>
2013-02-03 18:03:23 +0100

    start one2many storage adapter

---
+++ src/storage.sqlite.onetomany.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * SqliteStorageAdapter - manages model data in a SQLite database.
+ * @author dom <d.pesch@11com7.de>
+ * @since 2012-09-30
+ */
+var SqliteOneToManyStorageAdapter = (function($)
+{
+  var
+    _protectedColumns = ['id', 'dt_create', 'dt_change']
+    ;
+
+
+  //noinspection FunctionWithInconsistentReturnsJS
+  var SqliteOneToManyStorageAdapter = function(){
+    // scope-safe constructor
+    if (this instanceof SqliteOneToManyStorageAdapter)
+    {
+      this.dbQuery = null;
+    }
+    else
+    {
+      return new SqliteOneToManyStorageAdapter();
+    }
+  };
+
+  SqliteOneToManyStorageAdapter.prototype = new SqliteStorageAdapter;
+
+  
+  return SqliteStorageAdapter;
+})(jq);
