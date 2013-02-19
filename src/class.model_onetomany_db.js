1d275bb6ee47d45d659b14706f9908445338c35a
Dominik Pesch <d.pesch@11com7.de>
2013-02-19 22:26:37 +0100

    TODO: finish later!

--- src/class.model_onetomany_db.js
+++ src/class.model_onetomany_db.js
@@ -37,6 +37,7 @@ function($, window, undefined)
   {
     this._childs = [];
     this._childsChanged = {};
+    this._childsLoaded = false;
     this._childClass = undefined;
     this._childForeignKey = undefined;
     this._childOrder = undefined;
@@ -93,9 +94,92 @@ function($, window, undefined)
     if ($.isFunction(callback)) { callback(obj); }
   };
 
+
+  // --------------------------------------------------------------------------------
+  // getChilds
+  // --------------------------------------------------------------------------------
+  $.mvc.modelOneToManyDb.prototype.getChilds = function(callback)
+  {
+
+  };
+
+
+  // --------------------------------------------------------------------------------
+  // getChild
+  // --------------------------------------------------------------------------------
+  $.mvc.modelOneToManyDb.prototype.getChild = function(id, callback)
+  {
+
+  };
+
+
+
   // ====================================================================================================
   // Child helper methods
   // ====================================================================================================
+  // --------------------------------------------------------------------------------
+  // _findChildPos / _childExists
+  // --------------------------------------------------------------------------------
+  /**
+   * Returns the internal array index for a child with a given id; -1 if the child couldn't be found.
+   * The caller has to guarantee that the childs are loaded.
+   * @param id database id of the child
+   * @return {Number} array index (0...n-1) OR -1 for non existing childs
+   * @private
+   */
+  $.mvc.modelOneToManyDb.prototype._findChildPos = function(id)
+  {
+    for (var t = 0; t < this._childs.length; t++)
+    {
+      if (this._childs[t].id === id)
+      {
+        return t;
+      }
+    }
+
+    return -1;
+  };
+
+  /**
+   * (internal) checks if a child exists.
+   * The caller has to guarantee that the childs are loaded.
+   * @param id database id of the child
+   * @return {Boolean} TRUE if the child exists; else FALSE
+   * @private
+   */
+  $.mvc.modelOneToManyDb.prototype._childExists = function(id)
+  {
+    return (this._findChildPos(id) !== -1);
+  };
+
+
+  // --------------------------------------------------------------------------------
+  // _loadChilds
+  // --------------------------------------------------------------------------------
+  /**
+   * (internal) load all childs for a parent object.
+   * @param {Function} callback will be called on success
+   * @param {Boolean} [reload] will fore a reload on TRUE
+   * @private
+   */
+  $.mvc.modelOneToManyDb.prototype._loadChilds = function(callback, reload)
+  {
+    if (reload === true) { this._childsLoaded = false; }
+    if (!this.isNew() && !this._childsLoaded)
+    {
+      var obj = new this._childClass(), self = this;
+      obj.search(
+        { filter: [this._childForeignKey, "=", this.id], order: this._childForeignKey },
+        function(results)
+        {
+          self._childs = results;
+          self._childsChanged = {};
+          self._childsLoaded = true;
+          if ($.isFunction(callback)) { callback(self._childs); }
+        });
+    }
+    else if ($.isFunction(callback)) { callback(this._childs); }
+  };
 
 
 
