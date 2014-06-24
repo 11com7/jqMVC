edeab5f7120564f3934c7e527133f74a62addb0f
Dominik Pesch <d.pesch@11com7.de>
2014-06-24 12:32:00 +0200

    added restore()

--- src/class.db_backup.js
+++ src/class.db_backup.js
@@ -68,7 +68,7 @@
        */
       dump : function(successCallback, opts)
       {
-        opts = $.extend({}, opts);
+        opts = $.isObject(opts) ? opts : {};
 
         var
           self = this,
@@ -164,6 +164,189 @@
         }
       },
 
+
+      /**
+       *
+       * @param {String|Object} dump
+       * @param {function} successCallback
+       * @param {function(Number|String, String)} errorCallback
+       * @param {Object} [opts]
+       */
+      restore : function(dump, successCallback, errorCallback, opts)
+      {
+        opts = $.isObject(opts) ? opts : {};
+
+
+        // --------------------------------------------------------------------------------
+        // prepare data
+        // --------------------------------------------------------------------------------
+        var data = this._prepareData(dump, _error);
+        if (!data) { return; }
+
+
+        // --------------------------------------------------------------------------------
+        // check data
+        // --------------------------------------------------------------------------------
+        if (this._checkData(data, _error) !== true) { return; }
+
+
+        // --------------------------------------------------------------------------------
+        // restore data
+        // --------------------------------------------------------------------------------
+        var self = this, db = this._$db;
+        db.getDatabase().transaction(
+          function(tx)
+          {
+            var t;
+
+            // create tables
+            for (t = 0; t < data.sql.tables.length; t++)
+            {
+              db.executeSql(tx, data.sql.tables[t]);
+            }
+
+            // create triggers
+            for (t = 0; t < data.sql.triggers.length; t++)
+            {
+              db.executeSql(tx, data.sql.triggers[t]);
+            }
+
+            // create indexs
+            for (t = 0; t < data.sql.indexes.length; t++)
+            {
+              db.executeSql(tx, data.sql.indexes[t]);
+            }
+
+            // create views
+            for (t = 0; t < data.sql.views.length; t++)
+            {
+              db.executeSql(tx, data.sql.views[t]);
+            }
+
+            // insert data
+            if ($.isObject(data.data))
+            {
+              Object.keys(data.data).forEach(function(table)
+              {
+                var sql = 'INSERT OR IGNORE INTO ' + table
+                  + ' (' + data.tables[table].join(',') + ')'
+                  + ' VALUES (' + _repeat('?', data.tables[table].length, ',') + ')';
+
+                for (var t = 0; t < data.data[table].length; t++)
+                {
+                  db.executeSql(tx, sql, data.data[table][t]);
+                }
+              });
+            }
+
+          },
+          // TRANSACTION ERROR
+          function(/** SQLError */ error)
+          {
+            _error(error.code, error.message);
+          },
+          // TRANSACTION SUCCESS
+          function()
+          {
+            if ($.isFunction(successCallback)) { successCallback(); }
+          }
+        );
+
+
+        // --------------------------------------------------------------------------------
+        // helper
+        // --------------------------------------------------------------------------------
+        function _error(code, message)
+        {
+          if ($.isFunction(errorCallback))
+          {
+            errorCallback(code, message);
+          }
+          else
+          {
+            throw new Error('[' + code + ']: ' + message);
+          }
+        }
+
+        function _repeat(str, cnt, separator)
+        {
+          var back = [], t;
+          for (t = 0; t < cnt; t++)
+          {
+            back.push(str);
+          }
+          return back.join(separator);
+        }
+      },
+
+
+      /**
+       * (internal) returns dump as object (if possible).
+       * @param {String|Object} dump  (Object) will be returned
+       *                              (String) expects a JSON string which will be parsed
+       * @param {function(Number|String, String)} errorCallback will be called on errors (parse OR TypeError)
+       * @returns {$.DbBackup.Data|boolean}  (Object) dump as object
+       *                            (boolean) false if the data ca
+       * @private
+       */
+      _prepareData : function(dump, errorCallback)
+      {
+        if ($.isObject(dump))
+        {
+          return dump;
+        }
+        else if ('String' === _typeOf(dump))
+        {
+          try
+          {
+            return JSON.parse(dump);
+          }
+          catch (e)
+          {
+            errorCallback(e.name, e.message);
+          }
+        }
+        else
+        {
+          errorCallback('TypeError', 'unsupported dump type \'' + _typeOf(dump) + '\'');
+        }
+
+        return false;
+      },
+
+
+      /**
+       * @param {Object} data
+       * @param {function(Number|String, String)} errorCallback will be called on errors
+       * @returns {boolean}   TRUE if the essential internal structures exist; FALSE on error
+       * @private
+       */
+      _checkData : function(data, errorCallback)
+      {
+        if (!$.isObject(data)) { errorCallback('TypeError', 'data has to be object'); return false; }
+        if (!_checkObjectKeys(data, ['sql', 'tables'], 'data')) { return false; }
+        if (!_checkObjectKeys(data.sql, ['tables', 'indexes', 'triggers', 'views'], 'data.sql')) { return false; }
+
+        // should be ok
+        return true;
+
+        function _checkObjectKeys(obj, keys, errorPath)
+        {
+          return keys.every(function(key)
+          {
+            var keyExists = obj.hasOwnProperty(key) && $.isObject(obj[key]);
+            if (!keyExists)
+            {
+              errorCallback('Error', 'missing key \'' + key + '\' in ' + errorPath);
+            }
+            return keyExists;
+          });
+        }
+      },
+
+
+
+
       /**
        * @namespace
        */
@@ -243,7 +426,7 @@
 
       /**
        * sql-structure-backup.
-       * @type {{tables: Array, indexes: Array, views: Array}}
+       * @type {{tables: Array, triggers: Array, indexes: Array, views: Array}}
        */
       this.sql =
       {
@@ -274,4 +457,16 @@
       this.errors = {};
     };
 
+
+    /**
+     * returns the type of an object.
+     * @param {*} obj
+     * @return {String} class name [String|Number|Boolean|Date|Array|Object|Function|RegExp] OR undefined OR null
+     */
+    function _typeOf(obj)
+    {
+      if (obj === undefined) { return 'undefined'; }
+      if (obj === null) { return 'null'; }
+      return Object.prototype.toString.call(obj).slice(8, -1);
+    }
   })(af, window);
